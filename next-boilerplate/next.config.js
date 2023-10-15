a387bb081dfc0f525c4ee4ec38b640f3294e987b
miltonbolonha <miltonbolonha@gmail.com>
2023-10-15 09:22:36 -0300

    update repo

--- next-boilerplate/next.config.js
+++ next-boilerplate/next.config.js
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.GITHUB_PAGES || false;
 const customDomain = "https://edu4.dev";
-const repo = null;
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
-  assetPrefix: isGithubPages ? customDomain : null,
+  assetPrefix: customDomain,
   images: {
     loader: "custom",
     loaderFile: "./src/containers/imgLoaderContainer.js",
   },
   env: {
     IS_GITHUB_PAGE: isGithubPages,
-    THEME_FOLDER: repo,
   },
 };
 
